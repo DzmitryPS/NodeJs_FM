5f298ce9b1736e80d1fc80e42701b7f3e32d0017
dima <galtd@list.ru>
2021-03-14 19:13:37 +0300

    comand line scripts

---
+++ index.js
@@ -0,0 +1,60 @@
+#! /C:/Users/Dima/env node
+
+"use strict";
+
+var util = require("util")
+var path = require("path")
+var fs = require("fs");
+
+var getStdin = require("get-stdin")
+
+var args = require("minimist")(process.argv.slice(2),{
+     boolean: ["help", "in"], 
+     string: ["file"]
+    })
+
+
+if (process.env.HELLO){
+    console.log(process.env.HELLO)
+}
+
+
+if(args.help){
+    console.log(args.help)
+}
+
+else if (args.in || args._.includes("-")){
+ getStdin().then(processFile).catch(error);
+}
+else if(args.file){
+    fs.readFile(path.resolve(args.file), function onContenst(err, contents){
+        if(err){
+            error(err.toString());
+        }
+        else{
+            processFile(contents.toString())
+        
+        }
+    })
+}
+else{
+    error("Incorrect usage.", true)
+}
+console.log(args)
+
+// process.stdout.write("hello world")
+// console.error(" oops")
+// process.stdin.read();
+
+function processFile(contents){
+    //   contents = contents.toString().toUpperCase
+    process.stdout.write(contents)
+}
+
+function error(msg, includeHelp = false){
+    console.error(msg);
+    if(includeHelp){
+        console.log("includeHelp")
+    }
+}
+
